4636645d5eef52e4e4a0d4f2eaa41c3165aa97f3
cwoodwarduk <calum.james.woodward@gmail.com>
2019-11-29 15:38:12 +0000

    doc blocs for ShoppingItemModel complete.

--- slim3-skeleton/src/classes/models/ShoppingItemModel.php
+++ slim3-skeleton/src/classes/models/ShoppingItemModel.php
@@ -4,13 +4,28 @@ namespace HealthyEating\models;
 
 class ShoppingItemModel
 {
+    /**
+     * @var $db a database connection
+     */
     private $db;
 
+    /**
+     * ShoppingItemModel constructor.
+     *
+     * @param $db a database connection.
+     *
+     * @return a populated $db property
+     */
     public function __construct($db)
     {
         $this->db = $db;
     }
 
+    /**
+     * Get all shopping items from the database
+     *
+     * @return an array of shopping items
+     */
     public function getAllShoppingItems()
     {
         $query = $this->db->prepare('SELECT `id`, `name`, `bought`, `deleted` FROM `shopping_items`;');
