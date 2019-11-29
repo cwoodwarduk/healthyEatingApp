d27610ee53dc5bcb74e7d9a4b761169f88581b6b
cwoodwarduk <calum.james.woodward@gmail.com>
2019-11-29 15:57:32 +0000

    greated function to add new items to database within the ShoppingItemModel, including doc blocs.

--- slim3-skeleton/src/classes/models/ShoppingItemModel.php
+++ slim3-skeleton/src/classes/models/ShoppingItemModel.php
@@ -33,4 +33,18 @@ class ShoppingItemModel
         $results = $query->fetchAll();
         return $results;
     }
+
+    /**
+     * Insert a new item into the database
+     *
+     * @param $name the name of the new shopping item
+     *
+     * @return boolean if query successful
+     */
+    public function addShoppingItem($name)
+    {
+        $query = $this->db->prepare('INSERT INTO `shopping_items` (`name`, `bought`, `deleted`) VALUES (:name, 0, 0);');
+        $query->bindParam(':name', $name);
+        return $query->execute();
+    }
 }
