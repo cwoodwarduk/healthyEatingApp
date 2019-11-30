284261d8b0d1d880077fab80300556d6aaf60ef0
cwoodwarduk <calum.james.woodward@gmail.com>
2019-11-30 11:17:39 +0000

    added delete buttons next to each item in display items helper.

--- slim3-skeleton/src/classes/helpers/DisplayShoppingItemsHelper.php
+++ slim3-skeleton/src/classes/helpers/DisplayShoppingItemsHelper.php
@@ -6,6 +6,13 @@ use HealthyEating\entities\ShoppingItemEntity;
 
 class DisplayShoppingItemsHelper
 {
+    /**
+     * Displays shopping items with a delete button next to each
+     *
+     * @param $data shopping items the user has saved
+     *
+     * @return string of html to display only those items that haven't been marked as bought or deleted.
+     */
     public static function displayShoppingItems($data){
         $output = '';
         foreach ($data as $shoppingItem){
@@ -16,6 +23,10 @@ class DisplayShoppingItemsHelper
             if ($bought == 0 && $deleted == 0){
                 $output .="<div class='shoppingItem'>
                             <h2>No." . $id . ": " . $name . "</h2>
+                            <form action='/delete' method='post'>
+                                <input name='id' value='" . $id . "' type='hidden'>
+                                <input class='button' type='submit' name='delete' value='delete'>
+                            </form>
                             </div>";
             }
         }
